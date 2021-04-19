9919a40fedc6ff7b53b9e6945f8c9e31b3ddabf1
Ossi Heino <ossi.heino@gmail.com>
2021-04-20 00:12:35 +0300

    ARduinO

--- NucleoArduino/temperature/temperature.ino
+++ NucleoArduino/temperature/temperature.ino
@@ -1,15 +1,27 @@
-char dataString[50] = {0};
-int a = 0;
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS 2
 
-void setup() {
-  // put your setup code here, to run once:
+// Setup a oneWire instance to communicate with any OneWire devices
+// (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+
+void setup(void)
+{
+  // start serial port
   Serial.begin(9600);
+  // Start up the library
+  sensors.begin();
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  a++;
-  sprintf(dataString, "%02X", a); // value2hex
-  Serial.println(dataString);   // send line
+void loop(void)
+{
+  // call sensors.requestTemperatures() to issue a global temperature
+  // request to all devices on the bus
+  sensors.requestTemperatures(); // Send the command to get temperature readings
+  Serial.println(sensors.getTempCByIndex(0)); // Why "byIndex"?
+  // You can have more than one DS18B20 on the same bus.
+  // 0 refers to the first IC on the wire
   delay(1000);
 }
